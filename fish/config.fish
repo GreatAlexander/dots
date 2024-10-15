6ed07113587db8e7a6115297a6b8e8c73e8f0fa8
Alejandro Bordallo <alex.bordallo@ed.ac.uk>
2024-10-15 15:03:54 +0100

    Add .local/bin (tmux) to Path and fix no newline

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -gx PATH $PATH /home/alex/.local/bin
     thefuck --alias | source
     starship init fish | source
 end
